79399b10ad89c648157b5a945377eebd9e08477d
TumiMoshou <139065767+TumiMoshou@users.noreply.github.com>
2024-05-05 09:28:07 +0200

    Update travelData array with personalised destinations and descriptions

--- data.js
+++ data.js
@@ -1,11 +1,33 @@
-export default [
+const travelData = [
     {
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        id: 1,
+        title: "Egypt",
+        location: "Giza",
+        googleMapsUrl: "https://maps.app.goo.gl/CkMedK3N3JJ17Rmu8",
+        startDate: "01 Mar, 2019",
+        endDate: "15 Mar, 2019",
+        description: "Exploring the pyramids of Giza was a breathtaking experience. Standing before these ancient wonders, I felt a deep connection to history. A truly unforgettable adventure.",
+        imageUrl: "https://unsplash.com/photos/brown-camel-near-pyramid-under-clear-blue-sky-69qT-N3L5sc"
+    },
+    {
+        id: 2,
+        title: "Hungary",
+        location: "Budapest",
+        googleMapsUrl: "https://maps.app.goo.gl/XLkfbcQL45yLRkNt8",
+        startDate: "05 Aug, 2020",
+        endDate: "20 Aug, 2020",
+        description: "Budapest captivated me with its stunning architecture and vibrant culture. The warmth and hospitality of the locals made my stay incredibly enjoyable!",
+        imageUrl: "https://unsplash.com/photos/view-of-gray-building-near-body-of-water-ezUDjYy17EA"
+    },
+    {
+        id: 3,
+        title: "Italy",
+        location: "Venice",
+        googleMapsUrl: "https://maps.app.goo.gl/34kbLCfZV7CmQ1888",
+        startDate: "12 Dec, 2021",
+        endDate: "26 Dec, 2021",
+        description: "The gondola ride through the waters was a highlight, offering a unique perspective of this beautiful city. The rich history and romantic charm of Venice made every moment feel special.",
+        imageUrl: "https://unsplash.com/photos/rialto-bridge-venice-italy-hFXZ5cNfkOk"
     }
-]
+];
+export default travelData;
