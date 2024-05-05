4c117d612693c6ac9b19b2c8b2736e6875d51ef7
TumiMoshou <139065767+TumiMoshou@users.noreply.github.com>
2024-05-05 09:29:05 +0200

    import styles.css file

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import travelData from './data.js';
+import './style.css';
 
 function App() {
     const entries = travelData.map(entry => {
         return (
-            <div key={entry.id}>
+            <div key={entry.id} className="entry">
+                <img src={entry.imageUrl} alt={entry.title} />
                 <h2>{entry.title}</h2>
                 <p>{entry.location} - <a href={entry.googleMapsUrl}>View on Google Maps</a></p>
                 <p>{entry.startDate} - {entry.endDate}</p>
                 <p>{entry.description}</p>
-                <img src={entry.imageUrl} alt={entry.title} />
             </div>
         );
     });
